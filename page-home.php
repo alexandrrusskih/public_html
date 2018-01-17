e015954965035a08267835ae7e5268c33f4d9e09
alexandrrusskih <30488026+alexandrrusskih@users.noreply.github.com>
2018-01-17 16:54:00 +0400

    db conect

--- page-home.php
+++ page-home.php
@@ -2,22 +2,13 @@
 /*
   * Template name: Home
   * */
-get_header();
+$qr_result   = "select * from main_sup ORDER BY date DESC  LIMIT " . strval($current_row) . "," . strval($screen_rows);
+$query = mysql_query($qr_result) or die("<p>Невозможно выполнить запрос: " . mysql_error() . ". Ошибка произошла в строке " . __LINE__ . "</p>");
 
-if(ale_get_meta('descr1')){
-	echo ale_get_meta('descr1');
-}
 
  //echo "Home page";
+?>
 
-$custom_query = new WP_Query( array( 'post_type' => 'services','posts_per_page'=>'1' ) );
-
-if ($custom_query->have_posts()) : while ($custom_query->have_posts()) : $custom_query->the_post(); ?>
-<div class="h2" ><?php the_title(); ?></div>
-<div class="contact-content">
-	<?php the_content(); ?>
-</div>
-<?php endwhile; endif; ?>
 
 <div class="wrapper content">
 	<img class="image"  src="/uploads/site_article/wed_rings/017_1.jpg" alt=""> 
